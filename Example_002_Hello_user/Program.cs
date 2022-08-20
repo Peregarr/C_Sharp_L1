09c0b5164e94c78b0145ab30e6022b0a9aa84556
Someone <someone@example.com>
2022-08-20 16:49:52 +0300

    002 hello user

---
+++ Example_002_Hello_user/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Enter your Name"); //Вывод данных
+string username = Console.ReadLine(); //Ожидание ввода данных в строку с переменной username
+Console.Write("Привет, ");
+Console.Write(username);
