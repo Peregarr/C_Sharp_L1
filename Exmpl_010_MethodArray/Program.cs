68bce4df1ccebaf436daf1409ddc7905b0fa19c5
Someone <someone@example.com>
2022-08-23 17:11:16 +0300

    010 while, find index

--- Exmpl_010_MethodArray/Program.cs
+++ Exmpl_010_MethodArray/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arr = { 15, 21, 39, 12, 23, 33, 13, 23, 313 };
+
+int n = arr.Length;
+int find = 33;
+
+int index = 0;
+while(index < n)
+{
+    if(arr[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // прервать в случае нахождения искомого, в ином случае цикл будет повторяться пока не пройдет весь массив
+                // и будет выводить все одинаковые элементы которые указаны в переменной find
+    }
+    index++;
+}
